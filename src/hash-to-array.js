d4ad23f45bb47a290041417e9ae41f929b7657d0
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2021-06-04 06:32:54 +0800

    Sort output

--- src/hash-to-array.js
+++ src/hash-to-array.js
@@ -1,6 +1,7 @@
 function h2a(hash, name_key, name_value) {
-    let array = []
-    for (const [key, value] of Object.entries(hash)) {
+    let array   = []
+    let entries = Object.entries(hash).sort()
+    for (const [key, value] of entries) {
         let object         = {}
         object[name_key]   = key
         object[name_value] = value
